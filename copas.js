0d30717ae7218fe1a363727ef9b327c78a41bead
devrsds <ryansouzads12@gmail.com>
2025-11-15 10:38:07 -0400

    Update copas.js

--- copas.js
+++ copas.js
@@ -2,28 +2,28 @@ const copas = [1958,1962,1970,1994]
 let quantidade
 do{
 quantidade = prompt("Insira um numero:\n1.Inserir no inicio\n2.Remover do inicio\n3.Inserir no final\n4.Remover no final\n5. Informar o tamanho")
-switch(quantidade){
-    case"1":
-copas.unshift()
-    document.writeln("<b><br>Após unshift:</br></b>",copas)
-break;
-    case "2":
-copas.shift()
-document.writeln("<b><br>Após shift:</br></b>",copas)
-    break;
-    case "3":
-copas.push()
-document.writeln("<b><br>Após push:</br></b>",copas)
-    break;
-    case"4":
-copas.pop()
-document.writeln("<b><br>Após remover o final:</br></b>",copas)
-    break;
-    case"5":
-copas.length()
-    break;
-    default:
-    alert("<br>Opção Invalida.</br>")
+    switch(quantidade){
+        case"1":
+        copas.unshift("1954")
+            document.writeln("<b><br>Após unshift:</br></b>",copas)
+        break;
+        case "2":
+        copas.shift()
+            document.writeln("<b><br>Após shift:</br></b>",copas)
+        break;
+        case "3":
+        copas.push("2026")
+            document.writeln("<b><br>Após push:</br></b>",copas)
+        break;
+        case"4":
+        copas.pop()
+            document.writeln("<b><br>Após remover o final:</br></b>",copas)
+        break;
+        case"5":
+        copas.length()
+        break;
+        default:
+        alert("Opção Invalida.")
 }
-document.writeln("<b><br>Após Tamanho:</br></b>",copas)
+    document.writeln("<b><br>Após Tamanho:</br></b>",copas.length)
 }while(quantidade!==0)
